36c83c66bfb6c9a367e437e508eaa7b668299d0d
Pedram <pedram@roomvu.co>
2023-03-11 12:53:50 +0330

    resolved some issues

--- Controller/tweetController.py
+++ Controller/tweetController.py
@@ -9,25 +9,36 @@ class TweetController:
         self.twitterService = TwitterService()
     
     def get_users_tweets(self, users, date):
-        tweets = {}
-        for user in users:
-            tweets[user] = self.scraper.get_tweets(user,date)
-        return jsonify(tweets)
+        try:
+            tweets = {}
+            for user in users:
+                tweets[user] = self.scraper.get_tweets(user,date)
+            return jsonify({'status':'success','message':"users tweets fetched successfully",'data':tweets})
+        except:
+            return jsonify({'status':'faild','message':"error, something is wrong",'data':[]})
 
     def get_user_tweets(self, user, date):
-        tweets = self.scraper.get_tweets(user,date)
-        return jsonify(tweets)
+        try:
+            tweets = self.scraper.get_tweets(user,date)
+            return jsonify({'status':'success','message':"user tweets fetched successfully",'data':tweets})
+        except:
+            return jsonify({'status':'faild','message':"error, something is wrong",'data':[]})
 
     def get_audiences(self,user,date):
-        
-        audiences = self.scraper.get_account_audiences(user,date)
-        audiences_list = []
-        [audiences_list.append(audience['username']) for audience in audiences]
-        active_audiences = self.scraper.get_account_active_audiences(audiences_list)
-        
-        return jsonify({'audiences':audiences,'active_audiences':active_audiences})
+        try:
+            audiences = self.scraper.get_account_audiences(user,date)
+            audiences_list = []
+            [audiences_list.append(audience['username']) for audience in audiences]
+            active_audiences = self.scraper.get_account_active_audiences(audiences_list)
+            
+            return jsonify({'status':'success','message':"user audiences fetched successfully",'data':{'audiences':audiences,'active_audiences':active_audiences}})
+        except:
+            return jsonify({'status':'faild','message':"error, something is wrong",'data':[]})
 
     def get_sentiment(self,user,thread):
-        tweets = []
-        tweets = self.scraper.get_thread(user,thread)
-        return jsonify(tweets)
+        try:
+            tweets = []
+            tweets = self.scraper.get_thread(user,thread)
+            return jsonify({'status':'success','message':"user sentiments fetched successfully",'data':tweets})
+        except:
+            return jsonify({'status':'faild','message':"error, something is wrong",'data':[]})
